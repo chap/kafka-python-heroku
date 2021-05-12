2bb8300df0bbf8086fb54159f69962dac4816a27
Chap Ambrose <chapambrose@gmail.com>
2021-05-12 15:40:34 -0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -3,5 +3,5 @@ import kafka_helper
 
 producer = kafka_helper.get_kafka_producer()
 topic = os.environ['KAFKA_PREFIX'] + 'my-topic'
-print('sending message topic:%{topic}')
+print('sending message topic:%s', topic)
 producer.send(topic, value='my value')
