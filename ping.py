74737d7c98e4e258f9261abc461a475577110077
Chap Ambrose <chapambrose@gmail.com>
2021-05-12 15:10:07 -0500

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,13 +1,14 @@
 import kafka_helper
 
-# while True:
-producer = kafka_helper.get_kafka_producer()
-print('sending message')
-producer.send('connecticut-84549.my-topic', 'my key')
+while True:
+    producer = kafka_helper.get_kafka_producer()
+    print('sending message')
+    k = (1024).to_bytes
+    producer.send('connecticut-84549.my-topic', k)
 
-print('reading message')
-consumer = kafka_helper.get_kafka_consumer('connecticut-84549.my-topic')
-for message in consumer:
-    print(message)
+    print('reading message')
+    consumer = kafka_helper.get_kafka_consumer('connecticut-84549.my-topic')
+    for message in consumer:
+        print(message)
 
-# time.sleep(1000)
+time.sleep(1000)
