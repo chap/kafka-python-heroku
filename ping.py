995f2801076f3f958f56ddc75724109ce71cdbda
Chap Ambrose <chapambrose@gmail.com>
2021-05-12 14:47:46 -0500

    Update ping.py

--- ping.py
+++ ping.py
@@ -2,7 +2,7 @@ import kafka_helper
 
 while True:
   producer = kafka_helper.get_kafka_producer()
-  producer.send('connecticut-84549.my-topic', key='my key', value={'k': 'v'})
+  producer.send('connecticut-84549.my-topic', key='my key', value='my value')
 
   consumer = kafka_helper.get_kafka_consumer(topic='connecticut-84549.my-topic')
   for message in consumer:
