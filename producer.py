658555da9dc3054d230bd31e8950306b922f091b
Chap Ambrose <chapambrose@gmail.com>
2021-05-20 16:28:22 -0500

    log every producer

--- producer.py
+++ producer.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv()  # take environment variables from .env.
+# from dotenv import load_dotenv
+# load_dotenv()  # take environment variables from .env.
 
 import os
 import kafka_helper
@@ -7,8 +7,8 @@ import time
 
 producer = kafka_helper.get_kafka_producer()
 topic = os.environ['KAFKA_PREFIX'] + 'my-topic'
-print('sending message topic:', topic)
 
 while True:
+  print('sending message topic:', topic)
   producer.send(topic, value='my value')
   time.sleep(10)
